3938222fd90edabdf437d3e23b89827d13d568fc
XiXnice <kuzminov399@yandex.ru>
2022-07-27 00:18:17 +0300

    FIX

--- Script.sql
+++ Script.sql
@@ -1,32 +1,51 @@
-create table if not exists Title (
-	id SERIAL primary key,
-	name VARCHAR(20) not null
+CREATE TABLE IF NOT EXISTS Title (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL
 );
 
-create table if not exists Performer (
-	id SERIAL primary key,
-	name VARCHAR(60) not null,
-	title_id INTEGER not null references Title(id)
+CREATE TABLE IF NOT EXISTS Performer (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL
 );
 
-create table if not exists Album (
-	id SERIAL primary key,
-	name VARCHAR(20) not null,
-	performer_id INTEGER not null references Performer(id),
+CREATE TABLE IF NOT EXISTS Con_Titles_Per (
+	title_id INTEGER NOT NULL REFERENCES Title(id),
+	performer_id INTEGER NOT NULL REFERENCES Performer(id),
+	constraint tit_per primary key (title_id, performer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	year_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Con_Per_Albums (
+	performer_id INTEGER NOT NULL REFERENCES Performer(id),
+	album_id INTEGER NOT NULL REFERENCES Album(id),
+	constraint per_alb primary key (performer_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	duration INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Con_Albums_Tracks (
+	album_id INTEGER NOT NULL REFERENCES Album(id),
+	track_id INTEGER NOT NULL REFERENCES Track(id),
+	constraint alb_tra primary key (album_id, track_id)
+);
+
+CREATE TABLE IF NOT EXISTS Collection (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
 	year_of_release date not null
 );
 
-create table if not exists Track (
-	id SERIAL primary key,
-	name VARCHAR(20) not null,
-	duration INTEGER not null,
-	album_id INTEGER not null references Album(id)
-);
-
-create table if not exists Collection (
-	id SERIAL primary key,
-	name VARCHAR(20) not null,
-	year_of_release date not null,
-	track_id INTEGER not null references Track(id),
-	album_id INTEGER not null references Album(id)
+CREATE TABLE IF NOT EXISTS Con_Tracks_Col (
+	track_id INTEGER NOT NULL REFERENCES Track(id),
+	collection_id INTEGER NOT NULL REFERENCES Collection(id),
+	constraint tra_col primary key (track_id, collection_id)
 );
